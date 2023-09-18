46fe8ef0a91e846532823bfe76325713c718c78a
Jens A. Koch <jakoch@web.de>
2023-09-18 23:31:43 +0200

    use core.setFailed to fail action on error

--- src/main.ts
+++ src/main.ts
@@ -111,7 +111,8 @@ async function run(): Promise<void> {
     if (error instanceof Error) {
       errorMessage = error.message
     }
-    core.error(errorMessage)
+    core.error(errorMessage);
+    core.setFailed(errorMessage);
   }
 }
 
